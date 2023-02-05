6e9bf6457956672d2e448a43ff33cd56ed649a3b
Arv00316 <arv00316@hotmail.com>
2023-02-05 12:45:28 -0300

    empezar construyendo el tablero

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDnbRWROgmbKf31S2q24LwTElT6WC_-L3I",
+    authDomain: "quas-controller.firebaseapp.com",
+    projectId: "quas-controller",
+    storageBucket: "quas-controller.appspot.com",
+    messagingSenderId: "211142187206",
+    appId: "1:211142187206:web:1eb106e8a76bf3aa711fc7",
+    measurementId: "G-NEJ7WRNX1C"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
